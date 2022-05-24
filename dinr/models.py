84b6fe6d28789e6e3ebf823e9f33a691159632e6
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-05-23 21:56:56 -0400

    Fixed location of logo image in restaurant and updated matches to allow for empty

--- dinr/models.py
+++ dinr/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -32,26 +31,31 @@ class Restaurant(models.Model):
     (Other, 'Other'),
   ]
   rest_name = models.CharField("Restaurant Name", blank=False,max_length=150)
+  rest_logo = models.ImageField("Restaurant Logo", upload_to ='uploads/')
   rest_desc = models.TextField("Description and Details")
   food_type = models.CharField("Type of Food", max_length=15, choices=TYPES_OF_FOOD_CHOICES, default='None')
   rest_url = models.URLField("Website URL", blank=True)
   rest_phone = models.CharField("Phone Number", blank=True, max_length=20)
-  rest_logo = models.ImageField("Restaurant Logo")
+  rest_street = models.CharField('Steet Address',max_length=150)
+  rest_city = models.CharField('City', max_length=100)
+  rest_state = models.CharField('State', max_length=2)
+  rest_zip = models.CharField('Zip Code', max_length=10)
+  
 
   def __str__(self):
         return self.rest_name
 
 class User(models.Model):
   firstName = models.CharField('First Name',max_length=50)
-  lastName = models.CharField('Last Name', max_length=50)
+  lastName = models.CharField('Last Name', max_length=50, blank=True)
   email = models.CharField("Email", max_length=150)
   username = models.CharField('username', max_length=50, unique=True)
   password = models.CharField('Password', max_length=50)
-  street_loc = models.CharField('Steet Address',max_length=150 )
+  street_loc = models.CharField('Steet Address',max_length=150)
   city_loc = models.CharField('City', max_length=100)
   state_loc = models.CharField('State', max_length=2)
   zip_loc = models.CharField('Zip Code', max_length=10)
-  matches = models.ManyToManyField(Restaurant)
+  matches = models.ManyToManyField(Restaurant, blank=True)
 
   def __str__(self):
         return self.username
